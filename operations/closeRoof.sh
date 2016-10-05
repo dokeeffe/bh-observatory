0f122172d46c6f357c8985e09d4b4b63d515f04f
Derek <derek0207@gmail.com>
2016-10-05 20:35:22 +0100

    add close function

--- operations/closeRoof.sh
+++ operations/closeRoof.sh
@@ -4,11 +4,10 @@ CONNECTED=$((indi_eval -f '"Aldi Roof.CONNECTION.CONNECT"==1') 2>&1)
 echo $CONNECTED
 if [ $CONNECTED -eq 0 ]
 then
-    echo 'Connecting roof driver'
+    echo 'Connecting Roof driver'
     indi_setprop 'Aldi Roof.CONNECTION.CONNECT=On'
     sleep 10
 fi
-mpg123 resources/alarm.mp3
-indi_setprop 'Aldi Roof.DOME_MOTION.DOME_CW=On'
-indi_setprop 'Aldi Roof.DOME_MOTION.DOME_CCW=Off'
+indi_setprop 'Aldi Roof.DOME_MOTION.DOME_CW=Off'
+indi_setprop 'Aldi Roof.DOME_MOTION.DOME_CCW=On'
 
