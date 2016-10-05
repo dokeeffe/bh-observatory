0aada3f91986c722d987318abf1b6833ffb194a7
dokeeffe <derek0207@gmail.com>
2016-10-05 21:43:08 +0100

    solver function

--- data-reduction/solveAll.py
+++ data-reduction/solveAll.py
@@ -3,19 +3,27 @@ import os
 import fnmatch
 import subprocess
 from shutil import copyfile
+from shutil import rmtree
 
-
+rmtree('/tmp/solver', ignore_errors=True)
+os.mkdir('/tmp/solver')
 for root, dirnames, filenames in os.walk('/home/dokeeffe/Pictures/CalibratedLight'):
     for filename in fnmatch.filter(filenames, '*.fits'):
-        print 'solving'
-        copyfile(filename, '/tmp/solver/tosolve.fits')
-        # hdulist = fits.open(os.path.join(root, filename))
-		# header = hdulist[0].header
-		# ra = header['OBJCTRA']
-		# dec = header['OBJCTDEC']
-		# print 'Plate Solving Image: ' + filename
-		# focalLength = header.get('FOCALLEN',1800)
-		# binning = header['XBINNING']
-		# pixelSize = header['PIXSIZE1']
-		# arcSecPerPixel =  (pixelSize/focalLength) * 206.3 * binning
-		# subprocess.call(['solve-field', '--no-plots', '--no-fits2fits', '--resort', '--skip-solved', '-O', '-L', str(arcSecPerPixel*.95), '-H', str(arcSecPerPixel*1.05),'-u','app', '/tmp/solver/tosolve.fits'] )
+        print 'solving ' + os.path.join(root, filename)
+        temp_file =  os.path.join('/tmp/solver', filename)
+        copyfile(os.path.join(root, filename),temp_file)
+        hdulist = fits.open(temp_file)
+        header = hdulist[0].header
+        ra = header['OBJCTRA']
+        dec = header['OBJCTDEC']
+        print 'Plate Solving Image: ' + filename
+        focalLength = header.get('FOCALLEN',1800)
+        binning = header['XBINNING']
+        pixelSize = header['PIXSIZE1']
+        arcSecPerPixel =  (pixelSize/focalLength) * 206.3 * binning
+        # subprocess.call(['solve-field', '--no-plots', '--no-fits2fits', '--resort', '--skip-solved', '-O', '-L', str(arcSecPerPixel*.95), '-H', str(arcSecPerPixel*1.05),'-u','app', '/tmp/solver/tosolve.fits'] )
+        params = ['solve-field', '--no-plots', '--no-fits2fits', '--resort', '--skip-solved', '-O', '-L', str(arcSecPerPixel*.95), '-H', str(arcSecPerPixel*1.05),'-u','app', temp_file]
+        print params
+        # copy solved file back to overwrite original
+        rmtree('/tmp/solver', ignore_errors=True)
+        os.mkdir('/tmp/solver')
