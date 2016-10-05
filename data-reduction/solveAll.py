2e1ca10a3b2dd5ceae124e8757f0d16d69bde498
dokeeffe <derek0207@gmail.com>
2016-10-05 21:14:27 +0100

    Add scripts for solver

--- data-reduction/solveAll.py
+++ data-reduction/solveAll.py
@@ -1,15 +1,21 @@
 from astropy.io import fits
 import os
+import fnmatch
 import subprocess
+from shutil import copyfile
 
-files_in_dir = os.listdir(os.getcwd())
-for file_in_dir in files_in_dir:
-	if file_in_dir.endswith('fits'):
-		fitsimage = fits.open(file_in_dir)
-		hdr = fitsimage[0].header
-		print 'Plate Solving Image: ' + file_in_dir
-		focalLength = hdr.get('FOCALLEN',1995)
-		binning = hdr['XBINNING']
-		pixelSize = hdr['PIXSIZE1']
-		arcSecPerPixel =  (pixelSize/focalLength) * 206.3 * binning
-		subprocess.call(["solve-field", "--no-plots", "--no-fits2fits", "--resort", "--skip-solved", "-O", "-L", str(arcSecPerPixel*.95), "-H", str(arcSecPerPixel*1.05),"-u","app", file_in_dir] )
+
+for root, dirnames, filenames in os.walk('/home/dokeeffe/Pictures/CalibratedLight'):
+    for filename in fnmatch.filter(filenames, '*.fits'):
+        print 'solving'
+        copyfile(filename, '/tmp/solver/tosolve.fits')
+        # hdulist = fits.open(os.path.join(root, filename))
+		# header = hdulist[0].header
+		# ra = header['OBJCTRA']
+		# dec = header['OBJCTDEC']
+		# print 'Plate Solving Image: ' + filename
+		# focalLength = header.get('FOCALLEN',1800)
+		# binning = header['XBINNING']
+		# pixelSize = header['PIXSIZE1']
+		# arcSecPerPixel =  (pixelSize/focalLength) * 206.3 * binning
+		# subprocess.call(['solve-field', '--no-plots', '--no-fits2fits', '--resort', '--skip-solved', '-O', '-L', str(arcSecPerPixel*.95), '-H', str(arcSecPerPixel*1.05),'-u','app', '/tmp/solver/tosolve.fits'] )
