7db748821f28293ab649babd49adce95dc3a8c54
Derek <derek0207@gmail.com>
2016-11-16 00:46:27 +0000

    add switcher

--- operations/power-switcher/runserver.py
+++ operations/power-switcher/runserver.py
@@ -7,13 +7,15 @@ import time
 from bottle import route, run, debug, error, abort
 
 socket_to_relay_map = {'mount':7,'ccd':6,'filterwheel':5,'heaters':4,'focuser':3,'aux':2,'weatherstation':1}
-socket_state_map = {'mount':'OFF','ccd':'OFF','filterwheel':'OFF','heaters':'OFF','focuser':'OFF','aux':'OFF','weatherstation':'OFF'}
+socket_state_map = {'mount':'OFF','ccd':'OFF','filterwheel':'OFF','heaters':'OFF','focuser':'OFF','aux':'OFF','weatherstation':'ON'}
 valid_states = ['on','off']
 
 @route('/power/<socket>/<state>')
 def power_change_state(socket,state):
     if(state not in valid_states):
         abort(400, "Bad request. You need to specify the correct device and power state")
+    if(socket == 'weatherstation'):
+        state = flip_state(state)
     command = state.upper() + str(socket_to_relay_map[socket])
     send_command(board,command)
     socket_state_map[socket] = state.upper()
@@ -22,6 +24,12 @@ def power_change_state(socket,state):
 @route('/power')
 def power_query():
     return socket_state_map
+
+def flip_state(state):
+    if(state == 'on'):
+        return 'off'
+    elif(state == 'off'):
+        return 'on'
 
 def send_command(board, cmd=[]):
     data = util.str_to_two_byte_iter(cmd+"\0")
