ed39fe09a87cacb538ed65d20f109b01bd625167
Derek <derek0207@gmail.com>
2016-11-21 22:10:30 +0000

    net changes

--- operations/powerOffAllEquipment.sh
+++ operations/powerOffAllEquipment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl http://192.168.1.225:8080/power/ccd/off
-curl http://192.168.1.225:8080/power/filterwheel/off
-curl http://192.168.1.225:8080/power/mount/off
-curl http://192.168.1.225:8080/power/heaters/off
-curl http://192.168.1.225:8080/power/focuser/off
+curl http://192.168.2.225:8080/power/ccd/off
+curl http://192.168.2.225:8080/power/filterwheel/off
+curl http://192.168.2.225:8080/power/mount/off
+curl http://192.168.2.225:8080/power/heaters/off
+curl http://192.168.2.225:8080/power/focuser/off
 
 
--- operations/startIndiServer.sh
+++ operations/startIndiServer.sh
@@ -1,7 +1,3 @@
-!#/bin/bash
-
-# ./powerOnAllEquipment.sh
-
-#indiserver -v -l /home/dokeeffe indi_celestron_gps indi_asi_ccd indi_atik_ccd indi_ipfocuser qik_flat indi_atik_wheel
+#!/bin/bash
 
 nohup indiserver -v -l /home/dokeeffe indi_celestron_gps indi_aldiroof indi_asi_ccd indi_atik_ccd indi_ipfocuser indi_atik_wheel indi_cloud_rain_monitor &
