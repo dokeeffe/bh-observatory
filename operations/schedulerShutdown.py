b0adafbf38f96924777a8448de2259cf1da6d567
Derek OKeeffe <derek0207@gmail.com>
2016-11-29 23:46:30 +0000

    EKOS shutdown procedure

--- operations/schedulerShutdown.py
+++ operations/schedulerShutdown.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 import ConfigParser
+import os
+
 import PyIndi
-import time, logging
+import time
 import sms
 
 
-class IndiClient(PyIndi.BaseClient):
+class ObservatoryShutdownIndiClient(PyIndi.BaseClient):
+    '''
+    Simple Indi client used to communicate with telescope, roof and ccd drivers for a shutdown procedure
+    '''
     def __init__(self):
-        super(IndiClient, self).__init__()
+        super(ObservatoryShutdownIndiClient, self).__init__()
 
     def newDevice(self, d):
         pass
@@ -44,6 +49,12 @@ class IndiClient(PyIndi.BaseClient):
         pass
 
     def safe_retry(self, func, arg):
+        '''
+        Utility function to safely retry indi functions. Indi functions dont always return the object on the first call as the device may not be ready
+        :param func:
+        :param arg:
+        :return:
+        '''
         result = func(arg)
         retry = 0
         while not (result) and retry < 10:
@@ -53,6 +64,11 @@ class IndiClient(PyIndi.BaseClient):
         return result
 
     def telescope_parked(self, telescope_name):
+        '''
+        Return true if the telescope is parked
+        :param telescope_name:
+        :return:
+        '''
         device_telescope = self.safe_retry(self.getDevice, telescope_name)
         telescope_connect = self.safe_retry(device_telescope.getSwitch, "CONNECTION")
         if not (device_telescope.isConnected()):
@@ -64,6 +80,11 @@ class IndiClient(PyIndi.BaseClient):
         return telescope_park[0].s == PyIndi.ISS_ON
 
     def close_roof(self, roof_name):
+        '''
+        Press the 'close' button on the roof passed. Works with indi rolloff simulator and custom roll off roofs only
+        :param roof_name:
+        :return:
+        '''
         device_roof = self.safe_retry(self.getDevice, roof_name)
         roof_connect = self.safe_retry(device_roof.getSwitch, "CONNECTION")
         if not (device_roof.isConnected()):
@@ -74,26 +95,77 @@ class IndiClient(PyIndi.BaseClient):
         roof_motion[0].s = PyIndi.ISS_OFF  # the "CONNECT" switch
         roof_motion[1].s = PyIndi.ISS_ON  # the "DISCONNECT" switch
         indiclient.sendNewSwitch(roof_motion)  # send this new value to the device
-        print 'closing'
+        while roof_motion[1].s == PyIndi.ISS_ON:
+            print 'waiting to close \n'
+            time.sleep(1)
+        print 'closing\n'
 
 
+
+def close_roof(roof_name, telescope_name):
+    '''
+    Safely close the roof. Send an SMS on close or on error
+    :param roof_name:
+    :param telescope_name:
+    :return:
+    '''
+    global indiclient
+    try:
+        indiclient = ObservatoryShutdownIndiClient()
+        indiclient.setServer("localhost", 7624)
+        if (not (indiclient.connectServer())):
+            print 'indi not running\n'
+            raise Exception('Exception: No indiserver running')
+        print 'shutting down3'
+        if indiclient.telescope_parked(telescope_name):
+            print('Telescope reports parked but this could also mean its parking so waiting 15sec before closing roof\n')
+            time.sleep(15)
+            indiclient.close_roof(roof_name)
+            send_sms('Roof Closed http://52-8.xyz/images/snapshot.jpg')
+        else:
+            print('Sending exception SMS. Scope not parked\n')
+            send_sms('Exception: cannot close roof as the telescope is not parked')
+    except Exception as e:
+        print('Sending exception SMS. Cause: \n' + e.message)
+        send_sms('ERROR: closing roof ' + e.message)
+        raise e
+
+
+def send_sms(message):
+    '''
+    Send an SMS message to operations manager
+    :param message:
+    :return:
+    '''
+    sms.send_sms(config.get('TEXTLOCAL_SMS', 'user'), config.get('TEXTLOCAL_SMS', 'apikey'),
+                 config.get('TEXTLOCAL_SMS', 'phonenumber'), message)
+
+
+def warm_ccd(ccd_name):
+    '''
+    Ensure the CCD temp is warm enough to shutdown
+    :param ccd_name:
+    :return:
+    '''
+    pass
+
+def poweroff():
+    '''
+    Poweroff all devices EXCEPT the mount.
+    The mount is connected to the main PC via USB which supplies 5v and must be powered down after the main PC shutsdown. Otherwise wierd shit happens.
+    :return:
+    '''
+    pass
+
+print 'Sending shutting down SMS\n'
 config = ConfigParser.ConfigParser()
-config.read('ops.cfg')
-print config.get('TEXTLOCAL', 'user')
-try:
-    indiclient = IndiClient()
-    indiclient.setServer("localhost", 7624)
-    if (not (indiclient.connectServer())):
-        raise Exception('Exception: No indiserver running on')
-    if indiclient.telescope_parked('Telescope Simulator'):
-        logging.info('Telescope reports parked but this could also mean parking so waiting 15sec before closing roof')
-        indiclient.close_roof('RollOff Simulator')
-        sms.send_sms(config.get('TEXTLOCAL', 'user'), config.get('TEXTLOCAL', 'apikey'),
-                     config.get('TEXTLOCAL', 'phonenumber'), 'Roof Closed')
-    else:
-        sms.send_sms(config.get('TEXTLOCAL', 'user'), config.get('TEXTLOCAL', 'apikey'),
-                     config.get('TEXTLOCAL', 'phonenumber'), 'Exception: cannot close roof as the telescope is not parked')
-except Exception as e:
-    sms.send_sms(config.get('TEXTLOCAL', 'user'), config.get('TEXTLOCAL', 'apikey'),
-                 config.get('TEXTLOCAL', 'phonenumber'), 'ERROR: closing roof ' + e.message)
-    raise e
+basedir = os.path.dirname(os.path.realpath(__file__))
+config.read(basedir+'/ops.cfg')
+send_sms('Starting observatory shutdown')
+close_roof(config.get('INDI_DEVICES', 'roof'),config.get('INDI_DEVICES', 'telescope'))
+warm_ccd(config.get('INDI_DEVICES', 'roof'))
+
+
+
+poweroff()
+
--- operations/sms.py
+++ operations/sms.py
@@ -2,8 +2,16 @@ import urllib      # URL functions
 import urllib2     # URL functions
 
 def send_sms(username, hash, number, message):
+    '''
+    Simple SMS sender based on textlocal.com
+    :param username:
+    :param hash:
+    :param number:
+    :param message:
+    :return:
+    '''
     sender = 'OBSERVATORY'
-    test_flag = 1
+    test_flag = 1  #1 for test 0 to send sms
     numbers = (number)
     values = {'test'    : test_flag,
               'uname'   : username,
@@ -21,5 +29,5 @@ def send_sms(username, hash, number, message):
         if response_url==url:
             print 'SMS sent!'
     except urllib2.URLError, e:
-        print 'Send failed!'
+        print 'SMS Send failed!'
         print e.reason
