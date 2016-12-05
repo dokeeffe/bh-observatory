adebcbc343cdd385c7d71f3c41b433d656c3c0e3
Derek OKeeffe <derek0207@gmail.com>
2016-12-05 20:24:17 +0000

    improve shutdown procedure

--- operations/ekos/roofInspector.py
+++ operations/ekos/roofInspector.py
@@ -2,9 +2,19 @@ from pyfirmata import Arduino, util
 import pyfirmata
 import time
 
-class RoofSwitchInspector:
 
-    NAME = "RoofSwitchInspector"
+class RollOffTimeoutException(Exception):
+    pass
+
+class RoofSwitchInspector(object):
+    '''
+    Responsible for connecting directly to the roof controller over USB/firmata.
+    Bypassing the INDI layer and going directly to the controllers open/close limit switches seems a safer option for safety checks
+    '''
+    NAME = 'RoofSwitchInspector'
+    OPEN = 'OPEN'
+    CLOSED = 'CLOSED'
+    MESSAGE_DELAY = 1
 
     def __init__(self, port):
         self.state = 'UNKNOWN'
@@ -26,16 +36,30 @@ class RoofSwitchInspector:
     def query(self):
         '''
         Send QUERY message to the roof device
-        :return:
+        :return: OPEN or CLOSED or UNKNOWN
         '''
         self.board.send_sysex(pyfirmata.pyfirmata.STRING_DATA, util.str_to_two_byte_iter('QUERY'))
-
-    def getstate(self):
-        self.query()
-        time.sleep(0.5) 
+        time.sleep(self.MESSAGE_DELAY) #necessary to wait a short time for the message to come back over usb.
         return self.state
 
+    def wait_until(self, required_state):
+        '''
+        Waits until the required state has been reached (OPEN/CLOSE)
+        :param required_state:
+        :return:
+        '''
+        retry = 0
+        while retry < 30:
+            if self.query() == required_state:
+                return
+            retry += 1
+        raise RollOffTimeoutException()
+
     def disconnect(self):
+        '''
+        Disconnect firmata from the device.
+        :return:
+        '''
         try:
             self.board.exit()
         except AttributeError:
