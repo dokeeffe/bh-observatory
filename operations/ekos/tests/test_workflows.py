12ed04dbafd9f584b15a1bd97bb6fccfa6936c3a
okeefd3 <derek.okeeffe@dell.com>
2016-12-06 17:09:47 +0000

    tests

--- operations/ekos/tests/test_workflows.py
+++ operations/ekos/tests/test_workflows.py
@@ -1,15 +1,13 @@
 from unittest import TestCase
-import ConfigParser
+import configparser
 from mock import Mock
 
-from ..workflows import StartupWorkflow
-
-
+from ..workflows import StartupWorkflow, ShutdownWorkflow
 
 class TestStartupWorkflow(TestCase):
 
-    def test_start_happypath(self):
-        config = ConfigParser.ConfigParser()
+    def test_start_roof_opens(self):
+        config = configparser.ConfigParser()
         config.read('ops.cfg')
         indi_client = Mock()
         roof_inspector = Mock()
@@ -19,8 +17,10 @@ class TestStartupWorkflow(TestCase):
         power_controller = Mock()
         wf = StartupWorkflow(indi_client, roof_inspector, message_sender, power_controller, config)
 
+        # Test
         wf.start()
 
+        # Assert
         roof_inspector.query.assert_called()
         indi_client.open_roof.assert_called_with('RollOff Simulator')
         message_sender.send_message.assert_called_with('Roof Open http://52-8.xyz/images/snapshot.jpg')
@@ -28,8 +28,8 @@ class TestStartupWorkflow(TestCase):
         indi_client.send_guide_pulse_to_mount.assert_called_with('Telescope Simulator')
         indi_client.set_ccd_temp.assert_called_with('CCD Simulator', -20)
 
-    def test_start_when_roof_does_not_open(self):
-        config = ConfigParser.ConfigParser()
+    def test_start_when_roof_does_not_open_then_exception_raised_and_message_sent(self):
+        config = configparser.ConfigParser()
         config.read('ops.cfg')
         indi_client = Mock()
         roof_inspector = Mock()
@@ -39,12 +39,89 @@ class TestStartupWorkflow(TestCase):
         power_controller = Mock()
         wf = StartupWorkflow(indi_client, roof_inspector, message_sender, power_controller, config)
 
+        # Test
         with self.assertRaises(Exception):
             wf.start()
 
+        # Assert
         roof_inspector.query.assert_called()
         indi_client.open_roof.assert_called_with('RollOff Simulator')
         indi_client.unpark_scope.assert_called_with('Telescope Simulator')
         indi_client.send_guide_pulse_to_mount.assert_called_with('Telescope Simulator')
         indi_client.set_ccd_temp.assert_called_with('CCD Simulator', -20)
         message_sender.send_message.assert_called_with('ERROR: in startup procedure Roof did not open')
+
+class TestShutdownWorkflow(TestCase):
+
+    def test_start_telescope_parked_then_roof_closes_ok(self):
+        config = configparser.ConfigParser()
+        config.read('ops.cfg')
+        indi_client = Mock()
+        indi_client_attrs = {'get_ccd_temp.return_value': 5}
+        indi_client.configure_mock(**indi_client_attrs)
+        roof_inspector = Mock()
+        roof_inspector_attrs = {'query.return_value': 'CLOSED'}
+        roof_inspector.configure_mock(**roof_inspector_attrs)
+        message_sender = Mock()
+        power_controller = Mock()
+        wf = ShutdownWorkflow(indi_client, roof_inspector, message_sender, power_controller, config)
+
+        # Test
+        wf.start()
+
+        # Assert
+        indi_client.close_roof.assert_called_with('RollOff Simulator')
+        roof_inspector.query.assert_called()
+        indi_client.set_ccd_temp.assert_called_with('CCD Simulator', -0.0)
+        message_sender.send_message.assert_called_with('Roof Closed http://52-8.xyz/images/snapshot.jpg')
+        power_controller.poweroff_equipment.assert_called()
+        power_controller.poweroff_pc.assert_called()
+
+    def test_start_telescope_parked_and_roof_does_not_close_then_exception_raised_and_message_sent(self):
+        config = configparser.ConfigParser()
+        config.read('ops.cfg')
+        indi_client = Mock()
+        indi_client_attrs = {'get_ccd_temp.return_value': 5}
+        indi_client.configure_mock(**indi_client_attrs)
+        roof_inspector = Mock()
+        roof_inspector_attrs = {'query.return_value': 'OPEN'}
+        roof_inspector.configure_mock(**roof_inspector_attrs)
+        message_sender = Mock()
+        power_controller = Mock()
+        wf = ShutdownWorkflow(indi_client, roof_inspector, message_sender, power_controller, config)
+
+        # Test
+        with self.assertRaises(Exception):
+            wf.start()
+
+        # Assert
+        indi_client.close_roof.assert_called_with('RollOff Simulator')
+        roof_inspector.query.assert_called()
+        indi_client.set_ccd_temp.assert_called_with('CCD Simulator', -0.0)
+        message_sender.send_message.assert_called_with('ERROR: closing roof: Roof did not close')
+        power_controller.poweroff_equipment.assert_called()
+        power_controller.poweroff_pc.assert_not_called()
+
+    def test_start_telescope_not_parked_then_roof_does_not_close_exception_raised_and_message_sent(self):
+        config = configparser.ConfigParser()
+        config.read('ops.cfg')
+        indi_client = Mock()
+        indi_client_attrs = {'get_ccd_temp.return_value': 5, 'telescope_parked': False}
+        indi_client.configure_mock(**indi_client_attrs)
+        roof_inspector = Mock()
+        roof_inspector_attrs = {'query.return_value': 'OPEN'}
+        roof_inspector.configure_mock(**roof_inspector_attrs)
+        message_sender = Mock()
+        power_controller = Mock()
+        wf = ShutdownWorkflow(indi_client, roof_inspector, message_sender, power_controller, config)
+
+        # Test
+        with self.assertRaises(Exception):
+            wf.start()
+
+        # Assert
+        indi_client.close_roof.assert_not_called()
+        indi_client.set_ccd_temp.assert_called_with('CCD Simulator', -0.0)
+        message_sender.send_message.assert_called_with('Exception: Roof Did not Close http://52-8.xyz/images/snapshot.jpg')
+        power_controller.poweroff_equipment.assert_called()
+        power_controller.poweroff_pc.assert_called()
--- operations/ekos/workflows.py
+++ operations/ekos/workflows.py
@@ -32,18 +32,19 @@ class StartupWorkflow(BaseWorkflow):
         '''
         try:
             if not self.indi_client.connectServer():
-                print 'indi not running\n'
+                print('indi not running ')
                 raise Exception('Exception: No indiserver running')
             self.indi_client.open_roof(self.roof_name)
-            self.message_sender.send_message('Roof Open http://52-8.xyz/images/snapshot.jpg')
             self.indi_client.unpark_scope(self.telescope_name)
             self.indi_client.send_guide_pulse_to_mount(self.telescope_name)
             self.indi_client.set_ccd_temp(self.ccd_name, -20)
             if self.roof_inspector.query() != 'OPEN':
                 raise Exception('Roof did not open')
+            else:
+                self.message_sender.send_message('Roof Open http://52-8.xyz/images/snapshot.jpg')
         except Exception as e:
-            print('Sending exception SMS. Cause: \n' + e.message)
-            self.message_sender.send_message('ERROR: in startup procedure ' + e.message)
+            self.message_sender.send_message('ERROR: in startup procedure ' + str(e))
+            print('Sending exception SMS. Cause: ' + e.message)
             raise e
 
 
@@ -52,16 +53,16 @@ class ShutdownWorkflow(BaseWorkflow):
     def start(self):
         '''
         Performs the following
-            close the roof
             warm ccd (may be done already but check to be safe)
             poweroff all equipment except mount
+            close the roof
             TODO: take necessary dark frames and calibrate data
             poweroff pc
         :return:
         '''
-        self._close_roof()
         self._warm_ccd()
         self.power_controller.poweroff_equipment()
+        self._close_roof()
         self.power_controller.poweroff_pc()
 
     def _close_roof(self):
@@ -71,19 +72,17 @@ class ShutdownWorkflow(BaseWorkflow):
         '''
         try:
             if self.indi_client.telescope_parked(self.telescope_name):
-                print('Telescope reports parked but this could also mean its parking so wait before closing roof ')
-                time.sleep(10)
                 self.indi_client.close_roof(self.roof_name)
                 if self.roof_inspector.query() == 'CLOSED':
                     self.message_sender.send_message('Roof Closed http://52-8.xyz/images/snapshot.jpg')
                 else:
-                    self.message_sender.send_sms('Exception: Roof Did not Close http://52-8.xyz/images/snapshot.jpg')
+                    raise Exception('Roof did not close')
             else:
                 print('Sending exception SMS. Scope not parked ')
-                self.message_sender.send_sms('Exception: cannot close roof as the telescope is not parked')
+                self.message_sender.send_message('Exception: cannot close roof as the telescope is not parked')
         except Exception as e:
-            print('Sending exception SMS. Cause: \n' + e.message)
-            self.message_sender.send_sms('ERROR: closing roof ' + e.message)
+            print('Sending exception SMS. Cause: ' + str(e))
+            self.message_sender.send_message('ERROR: closing roof: ' + str(e))
             raise e
 
     def _warm_ccd(self):
@@ -91,9 +90,9 @@ class ShutdownWorkflow(BaseWorkflow):
         Warm the CCD
         :return:
         '''
-        self.indiclient.set_ccd_temp(self.ccd_name, 0.0)
+        self.indi_client.set_ccd_temp(self.ccd_name, 0.0)
         retry = 0
-        while self.indiclient.get_ccd_temp(self.ccd_name) < -5 and retry < 300:
+        while self.indi_client.get_ccd_temp(self.ccd_name) < -5 and retry < 300:
             time.sleep(1)
             retry += 1
 
