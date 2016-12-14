2a91949ffe69850dd4a3a01b9c8dbaadf00a96d9
Derek OKeeffe <derek0207@gmail.com>
2016-12-14 20:07:39 +0000

    fix

--- operations/ekos/scheduler_workflow_script.py
+++ operations/ekos/scheduler_workflow_script.py
@@ -17,11 +17,11 @@ if __name__ == '__main__':
     config.read(basedir + '/ops.cfg')
     message_sender = SmsMessageSender(config.get('TEXTLOCAL_SMS', 'user'), config.get('TEXTLOCAL_SMS', 'apikey'),
                                       config.get('TEXTLOCAL_SMS', 'phonenumber'))
-    message_sender.send_message('Starting observatory startup procedure')
+    message_sender.send_message('Starting observatory '+sys.argv[1]+' procedure')
     try:
         indiclient = BhObservatoryIndiClient("localhost", 7624)
         indi_wrapper = BhObservatoryIndiAdapter(indiclient, config.get('INDI_DEVICES', 'roof'),
-                                             config.get('INDI_DEVICES', 'telescope'), config.get('INDI_DEVICES', 'ccd'))
+                                                config.get('INDI_DEVICES', 'telescope'), config.get('INDI_DEVICES', 'ccd'))
         power_controller = PowerController(config.get('DEVICES', 'powerswitcher_api'))
         if sys.argv[1] == 'startup':
             workflow = StartupWorkflow(indi_wrapper, message_sender, power_controller, config)
