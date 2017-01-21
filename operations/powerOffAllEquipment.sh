ae410ed56de6dbcc4ab2bffa72c93b173f2bb2c4
Derek <derek0207@gmail.com>
2017-01-21 20:12:29 +0000

    less noise

--- operations/powerOffAllEquipment.sh
+++ operations/powerOffAllEquipment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-curl http://192.168.2.225:8080/power/ccd/off
-curl http://192.168.2.225:8080/power/filterwheel/off
-curl http://192.168.2.225:8080/power/mount/off
-curl http://192.168.2.225:8080/power/heaters/off
-curl http://192.168.2.225:8080/power/focuser/off
-curl http://192.168.2.225:8080/power/aux/off
+curl http://192.168.2.225:8080/power/ccd/off > /dev/null 2>&1
+curl http://192.168.2.225:8080/power/filterwheel/off > /dev/null 2>&1
+curl http://192.168.2.225:8080/power/mount/off > /dev/null 2>&1
+curl http://192.168.2.225:8080/power/heaters/off > /dev/null 2>&1
+curl http://192.168.2.225:8080/power/focuser/off > /dev/null 2>&1
+curl http://192.168.2.225:8080/power/aux/off 
 
 
