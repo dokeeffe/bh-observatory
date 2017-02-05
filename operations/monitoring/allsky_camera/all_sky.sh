49e4381cad95a9aa2f96bc6b3daab2aaf4670036
Derek <derek0207@gmail.com>
2017-02-05 01:01:20 +0000

    allsky

--- operations/monitoring/allsky_camera/all_sky.sh
+++ operations/monitoring/allsky_camera/all_sky.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 export ZWO_ASI_LIB=/opt/asi/lib/x64/libASICamera2.so
 mkdir -p /tmp/allsky
 [[ $(indi_getprop | grep Roof.STATE) =~ "OPEN" ]] && python all_sky.py
-cp /tmp/allsky.jpg /tmp/allsky/allsky_$(date +%F_%R).jpg
+cp /tmp/allsky.jpg /tmp/allsky/allsky_$(date +%F_%H-%M).jpg
 scp /tmp/allsky.jpg dokeeffe@52-8.xyz:/var/www/html/images/.
