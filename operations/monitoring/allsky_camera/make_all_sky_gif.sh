ae707a22e8809edab11a8940224c38498b65973a
Derek <derek0207@gmail.com>
2017-02-15 23:06:39 +0000

    mods to all sky cam

--- operations/monitoring/allsky_camera/make_all_sky_gif.sh
+++ operations/monitoring/allsky_camera/make_all_sky_gif.sh
@@ -1,2 +1,3 @@
 !#/bin/bash
-convert -resize 25% -delay 5 -loop 0 allsky*.jpg allsky.gif
+convert -resize 25% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-25.gif
+convert -resize 50% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-50.gif
