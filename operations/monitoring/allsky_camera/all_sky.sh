370e348c31d2bf395d745da9fc37caf77b78a8b1
Derek <derek0207@gmail.com>
2017-02-15 23:06:28 +0000

    mods to all sky cam

--- operations/monitoring/allsky_camera/all_sky.sh
+++ operations/monitoring/allsky_camera/all_sky.sh
@@ -2,7 +2,12 @@
 
 cd "$(dirname "$0")"
 export ZWO_ASI_LIB=/opt/asi/lib/x64/libASICamera2.so
-mkdir -p /tmp/allsky
-[[ $(indi_getprop | grep Roof.STATE) =~ "OPEN" ]] && python all_sky.py
-cp /tmp/allsky.jpg /tmp/allsky/allsky_$(date +%F_%H-%M).jpg
-scp /tmp/allsky.jpg dokeeffe@52-8.xyz:/var/www/html/images/.
+mkdir -p ~/Pictures/allsky
+if [[ $(indi_getprop | grep Roof.STATE) =~ "OPEN" ]] 
+then
+    python all_sky.py
+else
+    python all_sky_day.py
+fi
+cp ~/Pictures/allsky/allsky.jpg ~/Pictures/allsky/allsky_$(date +%F_%H-%M).jpg
+scp ~/Pictures/allsky/allsky.jpg dokeeffe@52-8.xyz:/var/www/html/images/.
