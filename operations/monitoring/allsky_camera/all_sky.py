2df2c502f229465ee3963e2ecb2e738e324a490b
Derek <derek0207@gmail.com>
2017-02-27 21:54:01 +0000

    setting tat actually work for the asicam

--- operations/monitoring/allsky_camera/all_sky.py
+++ operations/monitoring/allsky_camera/all_sky.py
@@ -56,14 +56,13 @@ camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandW
 # the sensitivity, lens and lighting conditions used.
 camera.disable_dark_subtract()
 
-camera.set_control_value(asi.ASI_GAIN, 50)
+camera.set_control_value(asi.ASI_GAIN, 150)
 camera.set_control_value(asi.ASI_EXPOSURE, 20000000)
 camera.set_control_value(asi.ASI_WB_B, 99)
 camera.set_control_value(asi.ASI_WB_R, 75)
-camera.set_control_value(asi.ASI_GAMMA, 0)
+camera.set_control_value(asi.ASI_GAMMA, 50)
 camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
 camera.set_control_value(asi.ASI_FLIP, 0)
-camera.set_roi(bins=2)
 
 print('Enabling stills mode')
 try:
