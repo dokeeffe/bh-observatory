e9401640fa882206f3f2df1b3f34bc44e8b50118
dokeeffe <derek0207@gmail.com>
2017-04-25 21:34:28 +0100

    Turn back on sec camera light on shutdown

--- operations/ekos/schedulerShutdown.sh
+++ operations/ekos/schedulerShutdown.sh
@@ -9,6 +9,8 @@ curl http://192.168.2.225:8080/power/filterwheel/off > /dev/null 2>&1
 curl http://192.168.2.225:8080/power/mount/off > /dev/null 2>&1
 curl http://192.168.2.225:8080/power/heaters/off > /dev/null 2>&1
 curl http://192.168.2.225:8080/power/focuser/off > /dev/null 2>&1
-curl http://192.168.2.225:8080/power/aux/off 
+curl http://192.168.2.225:8080/power/aux/off > /dev/null 2>&1
+echo 'Switching ON IR Lamps'
+curl 'http://192.168.2.220/decoder_control.cgi?command=95' -H 'Authorization: Basic ZG9rZWVmZmU6ZG9rZWVmZmU=' > /dev/null 2>&1
 /usr/bin/python "$BASEDIR/scheduler_workflow_script.py" shutdown-complete
 
--- operations/monitoring/allsky_camera/make_all_sky_gif.sh
+++ operations/monitoring/allsky_camera/make_all_sky_gif.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#!/bin/bash
 convert -resize 25% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-25.gif
 convert -resize 50% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-50.gif
