ce689b12790a6a3a7cc8b2a9ceb82304121513e7
dokeeffe <derek0207@gmail.com>
2017-05-10 22:06:11 +0100

    Cleanup all sky camera capture script

--- operations/monitoring/allsky_camera/all_sky.py
+++ operations/monitoring/allsky_camera/all_sky.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os
-import time
 import zwoasi as asi
-
-
-__author__ = 'Steve Marple'
-__version__ = '0.0.17'
-__license__ = 'MIT'
-
-
-def save_control_values(filename, settings):
-    filename += '.txt'
-    with open(filename, 'w') as f:
-        for k in sorted(settings.keys()):
-            f.write('%s: %s\n' % (k, str(settings[k])))
-    print('Camera settings saved to %s' % filename)
-
 
 env_filename = os.getenv('ZWO_ASI_LIB')
 asi.init(env_filename)
@@ -26,30 +11,9 @@ if num_cameras == 0:
     sys.exit(0)
 
 cameras_found = asi.list_cameras()  # Models names of the connected cameras
-
-if num_cameras == 1:
-    camera_id = 0
-    print('Found one camera: %s' % cameras_found[0])
-else:
-    print('Found %d cameras' % num_cameras)
-    for n in range(num_cameras):
-        print('    %d: %s' % (n, cameras_found[n]))
-    # TO DO: allow user to select a camera
-    camera_id = 0
-    print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
-
+camera_id = 0
 camera = asi.Camera(camera_id)
 camera_info = camera.get_camera_property()
-
-# Get all of the camera controls
-print('')
-print('Camera controls:')
-controls = camera.get_controls()
-for cn in sorted(controls.keys()):
-    print('    %s:' % cn)
-    for k in sorted(controls[cn].keys()):
-        print('        %s: %s' % (k, repr(controls[cn][k])))
-
 
 # Use minimum USB bandwidth permitted
 camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MinValue'])
@@ -57,8 +21,7 @@ camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandW
 # Set some sensible defaults. They will need adjusting depending upon
 # the sensitivity, lens and lighting conditions used.
 camera.disable_dark_subtract()
-
-camera.set_control_value(asi.ASI_GAIN, 150)
+camera.set_control_value(asi.ASI_GAIN, 75)
 camera.set_control_value(asi.ASI_EXPOSURE, 20000000)
 camera.set_control_value(asi.ASI_WB_B, 99)
 camera.set_control_value(asi.ASI_WB_R, 75)
@@ -81,4 +44,3 @@ filename = '/home/dokeeffe/Pictures/allsky/allsky.jpg'
 camera.set_image_type(asi.ASI_IMG_RAW8)
 camera.capture(filename=filename)
 print('Saved to %s' % filename)
-#save_control_values(filename, camera.get_control_values())
