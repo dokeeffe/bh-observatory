e0902a7df9565a6c12c61c85572dffa1a1fb9771
Derek <derek0207@gmail.com>
2017-06-20 23:03:53 +0100

    timestamp allsky camera images

--- operations/monitoring/allsky_camera/make_all_sky_gif.sh
+++ operations/monitoring/allsky_camera/make_all_sky_gif.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 convert -resize 25% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-25.gif
-convert -resize 50% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-50.gif
+# convert -resize 50% -delay 5 -loop 0 ~/Pictures/allsky/allsky*.jpg ~/Dropbox/allsky-50.gif
+ffmpeg -framerate 15 -pattern_type glob -i '/home/dokeeffe/Pictures/allsky/allsky*.jpg' -c:v libx264 ~/Dropbox/allsky.mp4
