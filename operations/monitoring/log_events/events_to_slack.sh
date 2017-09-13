90127528e5322f32e0d3969f46f4fb38604bd932
Derek <derek0207@gmail.com>
2017-09-14 00:07:40 +0100

    hacking monitoring script

--- operations/monitoring/log_events/events_to_slack.sh
+++ operations/monitoring/log_events/events_to_slack.sh
@@ -2,7 +2,7 @@
 source ~/.slack.conf
 LATEST_LOG_FILE=$(find ~/.local/share/kstars/logs/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
 echo 'monitoring' $LATEST_LOG_FILE
-tail -f $LATEST_LOG_FILE | grep 'Aborting\|ailed\|candidate job\|Scheduler: Find next job\|Scheduler:  \"Slewing to\|Target is within acceptable range\|Autofocus in progress\|Autofocus complete after\|Shutdown complete\|Roof is open\|Mount is parked\|unparked\|Parking mount\|Starting guiding procedure\|Scheduler: Get next action\|DEBG - Capture:  \"Received image \|Cant contact weather device\|bad weather\|severe weather' | while read line; do
+tail -f $LATEST_LOG_FILE | grep --line-buffered 'Aborting\|ailed\|candidate job\|Scheduler: Find next job\|Scheduler:  \"Slewing to\|Target is within acceptable range\|Autofocus in progress\|Autofocus complete after\|Shutdown complete\|Roof is open\|Mount is parked\|unparked\|Parking mount\|Starting guiding procedure\|Scheduler: Get next action\|DEBG - Capture:  \"Received image \|Cant contact weather device\|bad weather\|severe weather' | while read line; do
     POST_MSG=$(eval echo $line | cut -d " " -f5-)
     echo $POST_MSG
     curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$POST_MSG"'"}' $WEBHOOK_URL
