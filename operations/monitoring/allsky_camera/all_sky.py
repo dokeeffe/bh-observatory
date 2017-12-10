1a87d443227eb491e053dd4448f34f632ca3643a
Derek <derek0207@gmail.com>
2017-12-10 12:46:17 +0000

    auto exposure all sky camera

--- operations/monitoring/allsky_camera/all_sky.py
+++ operations/monitoring/allsky_camera/all_sky.py
@@ -2,48 +2,69 @@
 from scipy import misc
 import sys, os
 import zwoasi as asi
+import pickle
+
+def capture(exposure_time, filename):
+    #cameras_found = asi.list_cameras()  # Models names of the connected cameras
+    camera_id = 0
+    camera = asi.Camera(camera_id)
+    camera_info = camera.get_camera_property()
+    # Use minimum USB bandwidth permitted
+    camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MinValue'])
+    camera.disable_dark_subtract()
+    camera.set_control_value(asi.ASI_GAIN, 75)
+    print('setting exposure to {} seconds'.format(float(exposure_time/1000000)))
+    camera.set_control_value(asi.ASI_EXPOSURE, exposure_time)
+    #camera.set_control_value(asi.ASI_WB_B, 99)
+    #camera.set_control_value(asi.ASI_WB_R, 75)
+    #camera.set_control_value(asi.ASI_GAMMA, 0)
+    #camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
+    #camera.set_control_value(asi.ASI_FLIP, 0)
+    try:
+        camera.stop_exposure()
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        pass
+    print('Capturing a single 8-bit mono image')
+    camera.set_image_type(asi.ASI_IMG_RAW8)
+    camera.capture(filename=filename)
 
 env_filename = os.getenv('ZWO_ASI_LIB')
+filename = '/home/dokeeffe/Pictures/allsky/allsky.jpg'
 asi.init(env_filename)
 num_cameras = asi.get_num_cameras()
 if num_cameras == 0:
     print('No cameras found')
     sys.exit(0)
 
-cameras_found = asi.list_cameras()  # Models names of the connected cameras
-camera_id = 0
-camera = asi.Camera(camera_id)
-camera_info = camera.get_camera_property()
-
-# Use minimum USB bandwidth permitted
-camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MinValue'])
-
-# Set some sensible defaults. They will need adjusting depending upon
-# the sensitivity, lens and lighting conditions used.
-camera.disable_dark_subtract()
-camera.set_control_value(asi.ASI_GAIN, 75)
-camera.set_control_value(asi.ASI_EXPOSURE, 10000000)
-# moonless night -> camera.set_control_value(asi.ASI_EXPOSURE, 25000000)
-#camera.set_control_value(asi.ASI_WB_B, 99)
-#camera.set_control_value(asi.ASI_WB_R, 75)
-#camera.set_control_value(asi.ASI_GAMMA, 0)
-#camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
-#camera.set_control_value(asi.ASI_FLIP, 0)
-
-
+exposures = [20000000, 10000000,1000000,100000,10000,1000]
+exposure_index = 0
+#loop until a good exposure found
 try:
-    camera.stop_exposure()
-except (KeyboardInterrupt, SystemExit):
-    raise
+    f = open('all_sky_exposure_index.pckl', 'rb')
+    exposure_index = obj = pickle.load(f)
+    f.close()
 except:
     pass
-
-print('Capturing a single 8-bit mono image')
-filename = '/home/dokeeffe/Pictures/allsky/allsky.jpg'
-camera.set_image_type(asi.ASI_IMG_RAW8)
-camera.capture(filename=filename)
-image_data = misc.imread(filename)
-if image_data.mean() > 200:
-    os.remove(filename)
-else:
-    print('Saved to %s' % filename)
+captured = False
+while not captured:
+    capture(exposures[exposure_index], filename)
+    image_data = misc.imread(filename)
+    print('Image average pixel = {}'.format(image_data.mean()))
+    if image_data.mean() > 200:
+        print('removing overexposed image')
+        os.remove(filename)
+        exposure_index+=1
+        capture(exposures[exposure_index], filename)
+    elif image_data.mean() < 100:
+        print('removing underexposed image')
+        os.remove(filename)
+        exposure_index-=1
+        capture(exposures[exposure_index], filename)
+    else:
+        print('Saved to %s' % filename)
+        captured = True
+        f = open('all_sky_exposure_index.pckl', 'wb')
+        pickle.dump(exposure_index, f)
+        f.close()
