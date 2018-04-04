574b15bd3499038933f8e11ee665c731cef30851
Derek <derek0207@gmail.com>
2018-04-04 22:24:40 +0100

    add missing script

---
+++ operations/jumphost-access/getcurrentweather_send_to_linode.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import time,os
+from subprocess import call
+import urllib
+
+urllib.urlretrieve('http://192.168.2.227:8080/weather/current', '/tmp/current.json')
+call(['scp', '/tmp/current.json', 'dokeeffe@52-8.xyz:/var/www/html/images/.'])
+urllib.urlretrieve('http://192.168.2.227:8080/weather/history', '/tmp/history.json')
+call(['scp', '/tmp/history.json', 'dokeeffe@52-8.xyz:/var/www/html/images/.'])
