f0336e09cd865804832a5bd060624f351460cc3a
Derek <derek0207@gmail.com>
2018-05-05 23:35:52 +0100

    improvements to scheduler startup script

--- operations/ekos/schedulerStartup.sh
+++ operations/ekos/schedulerStartup.sh
@@ -8,7 +8,8 @@ echo 'Switching OFF IR Lamps'
 curl 'http://192.168.2.220/decoder_control.cgi?command=94' -H 'Authorization: Basic ZG9rZWVmZmU6ZG9rZWVmZmU='
 echo 'Switching OFF Dehumidifier'
 /usr/bin/python "$BASEDIR/tplink_hs1xx/smartplug.py" $DEHUMIDIFIER OFF
-
+# set ccd cooler on
+indi_setprop "Atik 383L+ CCD.CCD_TEMPERATURE.CCD_TEMPERATURE_VALUE"=-20
 # Check park status of roof and scope, check xml files
 
 # Check weather
