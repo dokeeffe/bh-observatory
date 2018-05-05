78e5480eb4ec4c02befcce355fc45d29b8fb4d39
Derek <derek0207@gmail.com>
2018-05-05 23:36:34 +0100

    improve open roof sh

--- operations/open_roof.sh
+++ operations/open_roof.sh
@@ -3,26 +3,31 @@
 if [[ $(curl --silent http://192.168.2.227:8080/weather/current | jq -r '.rain') = *true* ]]; then
   echo "WEATHER: RAINING. Cannot open roof"
   exit 1
+else
+  echo "WEATHER: NOT RAINING"
 fi
 SKYTEMP=$(curl --silent http://192.168.2.227:8080/weather/current | jq -r '.skyTemp' | bc) 
 if [ $(echo "$SKYTEMP > -10" | bc) -ne 0 ] 
 then 
-  echo "WEATHER: CLOUDY:"
+  echo "WEATHER: CLOUDY"
+else
+  echo "WEATHER: CLEAR"
 fi
 
-echo "Opening roof"
-indi_setprop 'Aldi Roof.CONNECTION.CONNECT=Off'
+indi_setprop 'Aldi Roof.CONNECTION.CONNECT=Off'  || { echo 'indi_setprop failed. Is the indiserver running?' ; exit 1; }
 sleep 0.5
 indi_setprop 'Aldi Roof.CONNECTION.CONNECT=On'
 sleep 2
 if [[ $(indi_getprop 'Aldi Roof.STATE.State') = *OPEN* ]]; then
-  echo "ROOF ALREADY OPEN"
+  echo "ROOF ALREADY OPEN. EXITING"
   exit 1
 fi
 if [[ $(indi_getprop 'Aldi Roof.STATE.State') = *UNKNOWN* ]]; then
-  echo "ROOF STATE UNKNOWN EXITING"
+  echo "ROOF STATE UNKNOWN. EXITING"
   exit 1
 fi
+
+echo "Opening roof"
 indi_setprop 'Aldi Roof.DOME_PARK.UNPARK=On'
 OUTPUT=""; 
 while [ `echo $OUTPUT | grep -c OPEN` = 0 ]; do 
