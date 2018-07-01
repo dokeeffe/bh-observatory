5fb9c0d2d9ad3588bd631accc1b01601af51ebc2
Derek <derek0207@gmail.com>
2018-07-01 22:10:09 +0100

    net changes

--- operations/jumphost-access/getweatherchart_send_to_linode.py
+++ operations/jumphost-access/getweatherchart_send_to_linode.py
@@ -3,8 +3,8 @@ import time,os
 from subprocess import call
 import urllib
 
-urllib.urlretrieve('http://192.168.2.227:8080/weather/chart/temperature.png', '/tmp/temperature.png')
-urllib.urlretrieve('http://192.168.2.227:8080/weather/chart/cloud.png', '/tmp/cloud.png')
+urllib.urlretrieve('http://192.168.1.227:8080/weather/chart/temperature.png', '/tmp/temperature.png')
+urllib.urlretrieve('http://192.168.1.227:8080/weather/chart/cloud.png', '/tmp/cloud.png')
 
 call(['scp', '/tmp/temperature.png', 'dokeeffe@52-8.xyz:/var/www/html/images/.'])
 call(['scp', '/tmp/cloud.png', 'dokeeffe@52-8.xyz:/var/www/html/images/.'])
