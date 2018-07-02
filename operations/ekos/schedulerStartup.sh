85d5d00ce3a7f2586b58ec8bdb4da48c384ede36
dokeeffe <derek0207@gmail.com>
2018-07-03 00:01:50 +0100

    Changing networks. Change to all IPs. Really need to get rid of these IPs...

--- operations/ekos/schedulerStartup.sh
+++ operations/ekos/schedulerStartup.sh
@@ -5,11 +5,7 @@ echo "$BASEDIR"
 
 /usr/bin/python "$BASEDIR/message_senders.py" startup
 echo 'Switching OFF IR Lamps'
-<<<<<<< HEAD
 curl 'http://192.168.1.222/decoder_control.cgi?command=94' -H 'Authorization: Basic ZG9rZWVmZmU6ZG9rZWVmZmU='
-=======
-curl 'http://192.168.1.220/decoder_control.cgi?command=94' -H 'Authorization: Basic ZG9rZWVmZmU6ZG9rZWVmZmU='
->>>>>>> ecd170c4692d5dd7ccf10dd0400204ec4208e70b
 echo 'Switching OFF Dehumidifier'
 /usr/bin/python "$BASEDIR/tplink_hs1xx/smartplug.py" $DEHUMIDIFIER OFF
 # set ccd cooler on
