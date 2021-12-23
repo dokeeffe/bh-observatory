c92cf7e2329c61e509ddf53ed5b18611b2295588
dokeeffe <derek0207@gmail.com>
2021-12-23 21:46:27 +0000

    new roof control

---
+++ operations/roof-scripts/close.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import requests
+import time
+
+def block_until(desired_state: str):
+    for _ in range(20):
+        time.sleep(1)
+        resp = requests.get('http://192.168.1.228:8080/roof')
+        if resp.json().get('state') == desired_state:
+            return
+    raise ValueError
+
+def main():
+    requests.post(url='http://192.168.1.228:8080/close')    
+    block_until('CLOSED')    
+    print('OK')
+
+
+if __name__ == "__main__":
+    main()
---
+++ operations/roof-scripts/connect.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def main():
+    print('OK')
+
+
+if __name__ == "__main__":
+    main()
