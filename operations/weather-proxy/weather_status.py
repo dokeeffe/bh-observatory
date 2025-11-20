ce09660ceb130f827dbc2489f1a2595468f84f65
Derek <derek0207@gmail.com>
2025-11-20 18:13:10 +0000

    latest

--- operations/weather-proxy/weather_status.py
+++ operations/weather-proxy/weather_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
 def get_weather_status():
-
     s = requests.Session()
     retries = Retry(total=5,
                     backoff_factor=2,
                     status_forcelist=[ 500, 502, 503, 504 ])
-
     s.mount('http://', HTTPAdapter(max_retries=retries))
     weather_data = s.get('http://192.168.1.227:8080/weather/current')
     current_weather = weather_data.json()
@@ -19,7 +17,6 @@ def get_weather_status():
     ambient_temp = current_weather['outsideTemp']
     clear = 1
     sky_temp_delta = current_weather['outsideTemp'] - current_weather['skyTemp']
-
     if rain:
         clear = 0
         msg=f'Raining. sky:{sky_temp} ambient:{ambient_temp}'
@@ -34,7 +31,8 @@ def get_weather_status():
         msg=f'Cloudy!!!. sky:{sky_temp} ambient:{ambient_temp}'
     return clear, msg
 
-now=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S%z")
+# Updated to use datetime.now(timezone.utc) instead of deprecated datetime.utcnow()
+now = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
 try:
     clear, msg = get_weather_status()
 except:
@@ -42,5 +40,3 @@ except:
     msg = 'System problem getting weather data'
 finally:
     print(json.dumps({"timestamp_utc": now,"roof_status": {"open_ok": clear,"reasons": msg}}))
-
-
